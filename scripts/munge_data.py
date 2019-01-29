9d4e95493822cf4b8b545f2b9ff0507f4130397b
Emily Dolson <EmilyLDolson@gmail.com>
2019-01-29 11:47:38 -0500

    Add data extraction script

---
+++ scripts/munge_data.py
@@ -0,0 +1,36 @@
+import glob
+import os
+import pandas as pd
+
+all_data = []
+
+for path in glob.glob("*/[0-9]*"):
+    if not os.path.exists(path+"/phylodiversity.csv"):
+        continue
+
+    csvs = glob.glob(path+"*.csv")
+
+    local_dfs = []
+    for csvfile in csvs:
+        local_dfs.append(pd.read_csv(path+"/oee.csv", index_col="generation"))
+
+    df = pd.concat(local_dfs, axis=1)    
+    local_data = {}
+
+    with open(path + "/run.log") as run_log_file:
+        for line in run_log_file:
+            if line.startswith("0"):
+                break
+            elif not line.startswith("set"):
+                continue
+            line = line.split()
+            local_data[line[1]] = line[2]
+
+    for val in local_data:
+        df[val] = local_data[val]
+
+    all_data.append(df)
+
+res = pd.concat(all_data)
+
+res.to_csv("../data/all_data.csv")
