fd8a2cbe1934c9462890648e5f6e2d06bc0f57c3
emilydolson <emilyldolson@gmail.com>
2021-09-05 07:13:03 +0000

    Deploying to master from  @ 4f0aecd337cbce07860acf15899784a21c923168 🚀

--- scripts/count_convergence.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import sys
-import numpy as np
-
-df = pd.read_csv(sys.argv[1])
-print(df.groupby("phenotype").count())
--- scripts/de_dup_phylogeny.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import sys
-import numpy as np
-
-df = pd.read_csv(sys.argv[1])
-df.replace(float("inf"), np.nan, inplace=True)
-df = df.groupby("id").aggregate(max)
-df.to_csv("phylogeny.csv")
